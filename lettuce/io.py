d360481653b189b42d690383dc6e42b1d1377659
Mario Christopher Bedrunka <mario.bedrunka@gmail.com>
2019-09-17 15:40:12 +0200

    Initialisation of the VTK-Writer (manuall adjustment necessary)

--- lettuce/io.py
+++ lettuce/io.py
@@ -9,6 +9,7 @@ import sys
 import logging
 import numpy as np
 import torch
+from pyevtk.hl import *
 from matplotlib import pyplot as plt
 
 
@@ -16,11 +17,24 @@ def write_png(filename, array2d):
     pass
 
 
-def write_vtk(filename, grid, field_data):
+def write_vtk(filename, resolution, field_data, id):
     # https://vtk.org/Wiki/VTK/Writing_VTK_files_using_python
     # https://anaconda.org/e3sm/evtk
     # https://pypi.org/project/pyevtk/
-    raise NotImplementedError
+    data_1 = np.zeros((resolution[0], resolution[1], resolution[2]))
+    data_2 = np.zeros((resolution[0], resolution[1], resolution[2]))
+
+    u = field_data.astype('float64')
+    u = np.transpose(u, (2, 1, 0))
+
+
+    data_1[:, :, 0] = u[:, :, 0]
+    data_2[:, :, 0] = u[:, :, 1]
+
+    gridToVTK("/Users/mariobedrunka/Documents/10_science/10_lattice_boltzmann/10_simulation/10_lettuce/data/" + "output_grid_" + id, np.arange(0, resolution[0]), np.arange(0, resolution[1]),
+              np.arange(0, resolution[2]), pointData={"ux": data_1, "uy": data_2})
+
+    #raise NotImplementedError
 
 
 class ErrorReporter:
--- lettuce/simulation.py
+++ lettuce/simulation.py
@@ -1,7 +1,7 @@
 """Lattice Boltzmann Solver"""
 
 from timeit import default_timer as timer
-from lettuce import LettuceException
+from lettuce import LettuceException, io
 import pickle
 
 
@@ -33,12 +33,15 @@ class Simulation:
     def step(self, num_steps):
         """Take num_steps stream-and-collision steps and return performance in MLUPS."""
         start = timer()
-        for _ in range(num_steps):
+        for i in range(num_steps):
             self.i += 1
             self.f = self.streaming(self.f)
             self.f = self.collision(self.f)
             for reporter in self.reporters:
                 reporter(self.i, self.i, self.f)
+            if i % 10 == 0:
+                io.write_vtk("output_vtk", [self.flow.resolution, self.flow.resolution, 1], self.lattice.convert_to_numpy(self.collision.lattice.u(self.f)), str(i))
+                print(i)
         end = timer()
         seconds = end-start
         num_grid_points = self.lattice.rho(self.f).numel()

76128099e63bcd21b33038ae1d403d6a6a4cfb0c
Mario Christopher Bedrunka <mario.bedrunka@gmail.com>
2019-10-21 13:47:20 +0200

    Update Forcingscheme # efficiency

--- lettuce/force.py
+++ lettuce/force.py
@@ -7,19 +7,18 @@ class Guo:
         self.F = F
 
     def source_term(self, f, u):
-        e_index = [Ellipsis] + [None] * self.lattice.D
-        emu= self.lattice.e[e_index] - u
+        index = [Ellipsis] + [None] * self.lattice.D
+        emu= self.lattice.e[index] - u
         eu = self.lattice.einsum("ib,b->i", [self.lattice.e, u])
         eeu = self.lattice.einsum("ia,i->ia", [self.lattice.e, eu])
         emu_eeu= emu/(self.lattice.cs ** 2) + eeu/(self.lattice.cs ** 4)
         emu_eeuF = self.lattice.einsum("ia,a->i", [emu_eeu, self.F])
-        weemu_eeuF = self.lattice.einsum("i,i->i", [self.lattice.w, emu_eeuF])
+        weemu_eeuF = self.lattice.w[index] * emu_eeuF
         return (1-1/(2*self.tau)) * weemu_eeuF
 
     def u_eq(self, f, force):
-        F = self.lattice.einsum("a,a->a", [torch.ones(f[0:2].shape), force.F])
-        a = force.A * F / self.lattice.rho(f)
-        return a
+        index = [Ellipsis] + [None] * self.lattice.D
+        return force.A * force.F[index] / self.lattice.rho(f)
 
     @property
     def A(self):
@@ -35,7 +34,8 @@ class ShanChen:
         return 0
 
     def u_eq(self, f, force):
-        return force.A * self.lattice.einsum("a,a->a", [torch.ones(f[0:2].shape), force.F]) / self.lattice.rho(f)
+        index = [Ellipsis] + [None] * self.lattice.D
+        return force.A * force.F[index] / self.lattice.rho(f)
 
     @property
     def A(self):

8d639e414c3f5843c7bfac1bf264dc6068175303
Andreas Krämer <kraemer.research@gmail.com>
2019-11-16 07:42:09 -0500

    flow_by_name as a dictionary

--- lettuce/flows/__init__.py
+++ lettuce/flows/__init__.py
@@ -6,10 +6,8 @@ from lettuce.flows.taylorgreen import TaylorGreenVortex2D, TaylorGreenVortex3D
 from lettuce.flows.couette import CouetteFlow2D
 from lettuce.flows.poiseuille import PoiseuilleFlow2D
 
-
-def flow_by_name(flow_class=None):
-    flow_dictionary = {
-        "taylor2D": TaylorGreenVortex2D,
-        "poiseuille2D": PoiseuilleFlow2D,
-        "couette2D": CouetteFlow2D}
-    return flow_dictionary if flow_class is None else flow_dictionary[flow_class]
+flow_by_name = {
+    "taylor2D": TaylorGreenVortex2D,
+    "poiseuille2D": PoiseuilleFlow2D,
+    "couette2D": CouetteFlow2D
+}

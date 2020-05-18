6d8396b4b5f9d0c8bbb299d08d98f479eedead66
dominik.wilde <dominik.wilde@h-brs.de>
2020-05-18 15:46:58 +0200

    3D simulation

--- lettuce/flows/obstacle.py
+++ lettuce/flows/obstacle.py
@@ -66,6 +66,62 @@ class Obstacle2D(object):
                 #ZeroGradientOutletTop(np.abs(x) < 1e-3, self.units.lattice, direction=[1.0, 0.0]),
                 BounceBackBoundary(self.mask, self.units.lattice)]
 
+class Obstacle3D(object):
+
+    def __init__(self, resolution_x, resolution_y, resolution_z, reynolds_number, mach_number, lattice, char_length_lu):
+        self.resolution_x = resolution_x
+        self.resolution_y = resolution_y
+        self.resolution_z = resolution_z
+        self.units = UnitConversion(
+            lattice,
+            reynolds_number=reynolds_number, mach_number=mach_number,
+            characteristic_length_lu=char_length_lu, characteristic_length_pu=1,
+            characteristic_velocity_pu=1
+        )
+        self.mask = None
+
+    def initizialize_object(self, mask, lattice):
+        self.boundaries.append(BounceBackBoundary(mask, lattice))
+
+    def initial_solution(self, x):
+        return np.array([0 * x[0]], dtype=float), np.array(
+            [0 * x[0] + self.units.convert_velocity_to_lu(1.0), x[1] * 0.05, x[2]*0.05],
+            dtype=float)
+
+    def getMaxU(self, f, lattice):
+        u0 = (lattice.u(f)[0])
+        u1 = (lattice.u(f)[1])
+        u2 = (lattice.u(f)[2])
+        return torch.max(torch.sqrt(u0 * u0 + u1 * u1 + u2 * u2))
+
+
+    #def calcEnstrophy(self, f, lattice):
+     #   u0 = lattice.u(f)[0].cpu().numpy()
+    #    u1 = lattice.u(f)[1].cpu().numpy()
+     #   grad_u0 = np.gradient(u0)
+     #   grad_u1 = np.gradient(u1)
+    #    dx = self.units.convert_length_to_pu(1.0)
+     #   vorticity = np.sum((grad_u0[1] - grad_u1[0]) * (grad_u0[1] - grad_u1[0]))
+      #  vorticity *= dx ** lattice.D
+    #    return vorticity
+
+    @property
+    def grid(self):
+        x = np.linspace(0, 1, num=self.resolution_x, endpoint=False)
+        y = np.linspace(0, 1, num=self.resolution_y, endpoint=False)
+        z = np.linspace(0, 1, num=self.resolution_z, endpoint=False)
+        return np.meshgrid(x, y, z, indexing='ij')
+
+    @property
+    def boundaries(self):
+        x, y, z = self.grid
+        return [EquilibriumBoundaryPU(np.abs(x) < 1e-6, self.units.lattice, self.units, np.array(
+            [self.units.characteristic_velocity_pu, self.units.characteristic_velocity_pu * 0.0, self.units.characteristic_velocity_pu * 0.0])),
+                #ZeroGradientOutletRight(np.abs(y - 1) < 1e-3, self.units.lattice, direction=[1.0, 0.0]),
+                # ZeroGradientOutletBottom(np.abs(x-1) < 1e-3, self.units.lattice, direction=[1.0, 0.0]),
+                # ZeroGradientOutletTop(np.abs(x) < 1e-3, self.units.lattice, direction=[1.0, 0.0]),
+                BounceBackBoundary(self.mask, self.units.lattice)]
+
 
 class ZeroGradientOutletRight:
     def __init__(self, mask, lattice, direction):

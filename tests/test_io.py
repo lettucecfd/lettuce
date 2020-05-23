86b9586cea4d12ce20391057523712ed5f73006c
dominikwilde <55636700+dominikwilde@users.noreply.github.com>
2020-05-24 00:34:30 +0200

    Generic reporters for mass, enstrophy, energy (#17)
    * Added generic reporters for mass, enstrophy, energy, and maximum velocity
    
    * Update io.py
    
    Co-authored-by: Dominik Wilde <dominik.wilde@gmail.com>
    Co-authored-by: Andreas Krämer <30830507+Olllom@users.noreply.github.com>

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,8 +1,8 @@
 
 import pytest
 import os
-from lettuce import TaylorGreenVortex2D, Lattice, D2Q9, write_image, BGKCollision, StandardStreaming,Simulation,EnergyReporter
-from lettuce.io import write_vtk, VTKReporter
+from lettuce import TaylorGreenVortex2D, TaylorGreenVortex3D, Lattice, D3Q27, D2Q9, write_image, BGKCollision, StandardStreaming, Simulation
+from lettuce.io import write_vtk, VTKReporter,EnstrophyReporter,EnergyReporter,MaxUReporter,MassReporter
 import numpy as np
 
 
@@ -16,17 +16,21 @@ def test_write_image(tmpdir):
     assert os.path.isfile(tmpdir/"p.png")
 
 
-def test_energy_reporter(dtype_device):
+@pytest.mark.parametrize("Reporter", [EnstrophyReporter, EnergyReporter, MaxUReporter, MassReporter])
+@pytest.mark.parametrize("Case", [TaylorGreenVortex2D,TaylorGreenVortex3D])
+def test_generic_reporters(Reporter, Case, dtype_device):
     dtype, device = dtype_device
     lattice = Lattice(D2Q9, dtype=dtype, device=device)
-    flow = TaylorGreenVortex2D(16, 10000, 0.05, lattice=lattice)
+    flow = Case(16, 10000, 0.05, lattice=lattice)
+    if(Case==TaylorGreenVortex3D):
+        lattice = Lattice(D3Q27, dtype=dtype, device=device)
     collision = BGKCollision(lattice, tau=flow.units.relaxation_parameter_lu)
     streaming = StandardStreaming(lattice)
     simulation = Simulation(flow=flow, lattice=lattice, collision=collision, streaming=streaming)
-    kinE_reporter = EnergyReporter(lattice, flow, interval=1, out=None)
+    kinE_reporter = Reporter(lattice, flow, interval=1, out=None)
     simulation.reporters.append(kinE_reporter)
     simulation.step(2)
-    assert(np.asarray(kinE_reporter.out)[1,1] == pytest.approx(np.asarray(kinE_reporter.out)[0,1],abs=0.1))
+    assert(np.asarray(kinE_reporter.out)[1,1] == pytest.approx(np.asarray(kinE_reporter.out)[0,1],abs=1.0))
 
 
 def test_write_vtk(tmpdir):

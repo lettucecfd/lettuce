Fix: change calculation of enstrophy reporter and convert enstrophy to pu

lettuce/io.py
@@ -145,17 +145,20 @@ class EnstrophyReporter(GenericStepReporter):
     parameter = 'Enstrophy'
 
     def parameter_function(self,i,t,f):
-        u0 = self.lattice.u(f)[0].cpu().numpy()
-        u1 = self.lattice.u(f)[1].cpu().numpy()
+        u0 = self.flow.units.convert_velocity_to_pu(self.lattice.u(f)[0].cpu().numpy())
+        u1 = self.flow.units.convert_velocity_to_pu(self.lattice.u(f)[1].cpu().numpy())
         dx = self.flow.units.convert_length_to_pu(1.0)
         grad_u0 = np.gradient(u0,dx)
         grad_u1 = np.gradient(u1,dx)
-        vorticity = np.sum((grad_u0[1] - grad_u1[0]) * (grad_u0[1] - grad_u1[0]))
+        vorticity = np.sum((grad_u1[0] - grad_u0[1]) * (grad_u1[0] - grad_u0[1]))
         if (self.lattice.D == 3):
-            u2 = self.lattice.u(f)[2].cpu().numpy()
+            u2 = self.flow.units.convert_velocity_to_pu(self.lattice.u(f)[2].cpu().numpy())
             grad_u2 = np.gradient(u2, dx)
-            vorticity+=np.sum((grad_u2[1] - grad_u1[2]) * (grad_u2[1] - grad_u1[2])+((grad_u0[2] - grad_u2[0]) * (grad_u0[2] - grad_u2[0])))
-        return vorticity.item()
+            vorticity = np.sum((grad_u2[1] - grad_u1[2]) * (grad_u2[1] - grad_u1[2])) \
+                        + np.sum((grad_u0[2] - grad_u2[0]) * (grad_u0[2] - grad_u2[0])) \
+                        + np.sum((grad_u1[0] - grad_u0[1]) * (grad_u1[0] - grad_u0[1]))
+        enstrophy = self.flow.units.convert_length_to_pu(1.0) ** self.lattice.D * vorticity
+        return enstrophy.item()
 
 class MassReporter(GenericStepReporter):
     """Reports the total mass"""
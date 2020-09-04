45120f628b8898de8ce738eed662cb75c3c1b9f3
Andreas Krämer <30830507+Olllom@users.noreply.github.com>
2020-09-04 13:47:26 +0200

    Report zeroth step (#42)
    * invoke reporters before simulation step
    
    * invoke reporters from 0th step on
    
    * report after step and 0th

--- lettuce/simulation.py
+++ lettuce/simulation.py
@@ -18,7 +18,7 @@ class Simulation:
     Attributes
     ----------
     reporters : list
-        A list of reporters. Their call functions are invoked after every simulation step.
+        A list of reporters. Their call functions are invoked after every simulation step (and before the first one).
 
     """
     def __init__(self, flow, lattice, collision, streaming):
@@ -55,6 +55,8 @@ class Simulation:
     def step(self, num_steps):
         """Take num_steps stream-and-collision steps and return performance in MLUPS."""
         start = timer()
+        if self.i == 0:
+            self._report()
         for _ in range(num_steps):
             self.i += 1
             self.f = self.streaming(self.f)
@@ -62,13 +64,16 @@ class Simulation:
             self.f = torch.where(self.no_collision_mask, self.f, self.collision(self.f))
             for boundary in self.flow.boundaries:
                 self.f = boundary(self.f)
-            for reporter in self.reporters:
-                reporter(self.i, self.i, self.f)
+            self._report()
         end = timer()
         seconds = end-start
         num_grid_points = self.lattice.rho(self.f).numel()
         mlups = num_steps * num_grid_points / 1e6 / seconds
         return mlups
+
+    def _report(self):
+        for reporter in self.reporters:
+            reporter(self.i, self.i, self.f)
 
     def initialize(self, max_num_steps=500, tol_pressure=0.001):
         """Iterative initialization to get moments consistent with the initial velocity.

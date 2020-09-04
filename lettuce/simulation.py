19e97090b75b84a8744ff84a55633a770baaa049
Andreas Krämer <kraemer.research@gmail.com>
2020-09-04 13:35:22 +0200

    report after step and 0th

--- lettuce/simulation.py
+++ lettuce/simulation.py
@@ -18,7 +18,7 @@ class Simulation:
     Attributes
     ----------
     reporters : list
-        A list of reporters. Their call functions are invoked before every simulation step.
+        A list of reporters. Their call functions are invoked after every simulation step (and before the first one).
 
     """
     def __init__(self, flow, lattice, collision, streaming):
@@ -55,20 +55,25 @@ class Simulation:
     def step(self, num_steps):
         """Take num_steps stream-and-collision steps and return performance in MLUPS."""
         start = timer()
+        if self.i == 0:
+            self._report()
         for _ in range(num_steps):
-            for reporter in self.reporters:
-                reporter(self.i, self.i, self.f)
             self.i += 1
             self.f = self.streaming(self.f)
             #Perform the collision routine everywhere, expect where the no_collision_mask is true
             self.f = torch.where(self.no_collision_mask, self.f, self.collision(self.f))
             for boundary in self.flow.boundaries:
                 self.f = boundary(self.f)
+            self._report()
         end = timer()
         seconds = end-start
         num_grid_points = self.lattice.rho(self.f).numel()
         mlups = num_steps * num_grid_points / 1e6 / seconds
         return mlups
+
+    def _report(self):
+        for reporter in self.reporters:
+            reporter(self.i, self.i, self.f)
 
     def initialize(self, max_num_steps=500, tol_pressure=0.001):
         """Iterative initialization to get moments consistent with the initial velocity.

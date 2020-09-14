d20e42da81fdc672588bb5fefe181974709032c6
McBs <mario.bedrunka@gmail.com>
2020-09-14 11:33:03 +0200

    Add an energy-spectrum-reporter to lettuce for 2d and 3d.

--- lettuce/reporters.py
+++ lettuce/reporters.py
@@ -165,3 +165,58 @@ class EnstrophyReporter(GenericStepReporter):
                 + ((grad_u0[2] - grad_u2[0]) * (grad_u0[2] - grad_u2[0]))
             )
         return vorticity.item() * dx**self.lattice.D
+
+
+class SpectrumReporter(GenericStepReporter):
+    """Reports the energy spectrum of the velocity
+    _____
+    NOTES
+    spectrum = simulation.reporters[0].out
+    for i in range(len(spectrum)):
+        k = spectrum[i][1][0]
+        ek = spectrum[i][1][1]
+        plt.loglog(k, ek)
+    plt.show()
+    """
+    _parameter_name = 'Energy spectrum'
+
+    def parameter_function(self,i,t,f):
+        global ek
+        u = self.flow.units.convert_velocity_to_pu(self.lattice.u(f)).cpu().numpy()
+        dx = self.flow.units.convert_length_to_pu(1.0)
+
+        if self.lattice.D == 2:
+            kx = np.fft.fftfreq(self.flow.resolution, d=1 / self.flow.resolution)
+            ky = np.fft.fftfreq(self.flow.resolution, d=1 / self.flow.resolution)
+            kx, ky = np.meshgrid(kx, ky)
+            kk = np.sqrt(kx ** 2 + ky ** 2)
+            norm = dx / self.flow.resolution
+            u0h = np.fft.fftn(u[0] * norm, axes=(0,1))
+            u1h = np.fft.fftn(u[1] * norm, axes=(0,1))
+            ekin = (u0h.real ** 2 + u0h.imag ** 2 + u1h.real ** 2 + u1h.imag ** 2) * .5
+            ek = np.zeros(int(np.max(kk)))
+            k = np.zeros(int(np.max(kk)))
+            for wv in range(int(np.max(kk))):
+                ii, jj = np.where((kk > (wv - 0.5)) & (kk < (wv + 0.5)))
+                ek[wv] = np.sum(ekin[ii, jj])
+                k[wv] = wv
+
+        if self.lattice.D == 3:
+            kx = np.fft.fftfreq(self.flow.resolution, d=1 / self.flow.resolution)
+            ky = np.fft.fftfreq(self.flow.resolution, d=1 / self.flow.resolution)
+            kz = np.fft.fftfreq(self.flow.resolution, d=1 / self.flow.resolution)
+            kx, ky, kz = np.meshgrid(kx, ky, kz)
+            kk = np.sqrt(kx ** 2 + ky ** 2 + kz ** 2)
+            norm = self.flow.resolution * dx ** (-2) * np.sqrt(2 * np.pi)
+            u0h = np.fft.fftn(u[0], axes=(0, 1, 2)) / norm
+            u1h = np.fft.fftn(u[1], axes=(0, 1, 2)) / norm
+            u2h = np.fft.fftn(u[2], axes=(0, 1, 2)) / norm
+            ekin = (u0h.real ** 2 + u1h.real ** 2 + u2h.real ** 2 + u0h.imag ** 2 + u1h.imag ** 2 + u2h.imag ** 2) * .5
+            ek = np.zeros(int(np.max(kk)))
+            k = np.zeros(int(np.max(kk)))
+            for wv in range(int(np.max(kk))):
+                ii, jj, ll = np.where((kk > (wv - 0.5)) & (kk < (wv + 0.5)))
+                ek[wv] = np.sum(ekin[ii, jj, ll])
+                k[wv] = wv
+
+        return k, ek
--- tests/test_reporters.py
+++ tests/test_reporters.py
@@ -1,8 +1,8 @@
 
 import pytest
 import os
-from lettuce import TaylorGreenVortex2D, TaylorGreenVortex3D, Lattice, D3Q27, D2Q9, write_image, BGKCollision, StandardStreaming, Simulation
-from lettuce.reporters import write_vtk, VTKReporter,EnstrophyReporter,EnergyReporter,MaxUReporter
+from lettuce import TaylorGreenVortex2D, TaylorGreenVortex3D, Lattice, D3Q27, D2Q9, write_image, BGKCollision, StandardStreaming, Simulation, DecayingTurbulence2D
+from lettuce.reporters import write_vtk, VTKReporter,EnstrophyReporter,EnergyReporter,MaxUReporter,SpectrumReporter
 import numpy as np
 
 
@@ -55,3 +55,41 @@ def test_vtk_reporter(tmpdir):
     simulation.step(2)
     assert os.path.isfile(tmpdir/"output_00000000.vtr")
     assert os.path.isfile(tmpdir/"output_00000001.vtr")
+
+
+@pytest.mark.parametrize("Flow", [TaylorGreenVortex2D,TaylorGreenVortex3D,DecayingTurbulence2D])
+def test_EnergySpectrumReporter(tmpdir, Flow):
+    lattice = Lattice(D2Q9, device='cpu')
+    if Flow == TaylorGreenVortex3D:
+        lattice = Lattice(D3Q27, device='cpu')
+    flow = Flow(resolution=20, reynolds_number=1600, mach_number=0.01, lattice=lattice)
+    collision = BGKCollision(lattice, tau=flow.units.relaxation_parameter_lu)
+    streaming = StandardStreaming(lattice)
+    simulation = Simulation(flow=flow, lattice=lattice, collision=collision, streaming=streaming)
+    simulation.reporters.append(SpectrumReporter(lattice, flow, out=None))
+    simulation.reporters.append(EnergyReporter(lattice, flow, out=None))
+    simulation.step(1)
+    spectrum = simulation.reporters[0].out
+    energy = simulation.reporters[1].out
+
+    if Flow == DecayingTurbulence2D:
+        dx = flow.units.convert_length_to_pu(1.0)
+        kx = np.fft.fftfreq(flow.resolution, d=1 / flow.resolution)
+        ky = np.fft.fftfreq(flow.resolution, d=1 / flow.resolution)
+        kx, ky = np.meshgrid(kx, ky)
+        kk = np.sqrt(kx ** 2 + ky ** 2)
+        kk[0][0] = 1e-16
+        ek = (kk) ** 4 * np.exp(-2 * (kk / flow.k0) ** 2)
+        ek[0][0] = 0
+        ek /= np.sum(ek)
+        ek *= flow.ic_energy
+        ek_ref = np.zeros(int(np.max(kk)))
+        k = np.zeros(int(np.max(kk)))
+        for wv in range(int(np.max(kk))):
+            ii, jj = np.where((kk > (wv - 0.5)) & (kk < (wv + 0.5)))
+            ek_ref[wv] = np.sum(ek[ii, jj])
+        assert (spectrum[0][1][1] == pytest.approx(ek_ref, rel= 0.0, abs=0.1))
+    assert (energy[0][1] == pytest.approx(np.sum(spectrum[0][1][1]), rel= 0.1, abs=0.0))
+
+
+

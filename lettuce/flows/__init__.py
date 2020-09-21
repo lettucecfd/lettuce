f803b0c5e79b02abb29ce8fe9f12c62e7770fc19
McBs <34478736+McBs@users.noreply.github.com>
2020-09-21 14:50:55 +0200

    Refactoring of Decaying Turbulence (#45)
    * Fix Typo in README and cli.py
    
    * Add initialization of ek (energy spectrum 'ek' depending on wavenumber 'k') as property
    
    * Specification of the dimension of DecayTurb no longer necessary. It depends now on the stencil's dimension
    
    * Remove DecayingTurbulence2D/3D in test_simulation.py and repalce them with DecayingTurbulence
    
    * Syntax
    
    * Fix big in constructor, change Syntax (PEP-8) and add Decaying-Turublence to API-module
    
    * Update modules.rst
    
    Co-authored-by: Andreas Krämer <30830507+Olllom@users.noreply.github.com>

--- lettuce/flows/__init__.py
+++ lettuce/flows/__init__.py
@@ -6,12 +6,12 @@ from lettuce.flows.taylorgreen import TaylorGreenVortex2D, TaylorGreenVortex3D
 from lettuce.flows.couette import CouetteFlow2D
 from lettuce.flows.poiseuille import PoiseuilleFlow2D
 from lettuce.flows.doublyshear import DoublyPeriodicShear2D
-from lettuce.flows.decayingturbulence import DecayingTurbulence2D, DecayingTurbulence3D
+from lettuce.flows.decayingturbulence import DecayingTurbulence
 
 flow_by_name = {
     "taylor2D": TaylorGreenVortex2D,
     "poiseuille2D": PoiseuilleFlow2D,
     "shear2D": DoublyPeriodicShear2D,
     "couette2D": CouetteFlow2D,
-    "decaying2D": DecayingTurbulence2D
+    "decay": DecayingTurbulence
 }

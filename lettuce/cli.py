01e858df4d8902ae0737026d1716a35b5f7d22b4
McBs <34478736+McBs@users.noreply.github.com>
2020-09-23 11:03:46 +0200

    Update bechmark (#54)
    * Fix Typo in README and cli.py
    
    * Update flow_by_name for benchmark
    
    * More precise import of stencils
    
    * Syntax
    
    * use D3Q19 instead of D3Q27, syntax of profiler

--- lettuce/cli.py
+++ lettuce/cli.py
@@ -16,7 +16,7 @@ import torch
 import numpy as np
 
 import lettuce
-from lettuce import BGKCollision, StandardStreaming, Lattice, D2Q9
+from lettuce import BGKCollision, StandardStreaming, Lattice, D2Q9, D3Q27
 
 from lettuce import TaylorGreenVortex2D, Simulation, ErrorReporter, VTKReporter
 from lettuce.flows import flow_by_name
@@ -60,13 +60,15 @@ def benchmark(ctx, steps, resolution, profile_out, flow, vtk_out):
     """Run a short simulation and print performance in MLUPS.
     """
     # start profiling
-    profile = cProfile.Profile()
-    profile.enable()
+    if profile_out:
+        profile = cProfile.Profile()
+        profile.enable()
 
     # setup and run simulation
     device, dtype = ctx.obj['device'], ctx.obj['dtype']
-    lattice = Lattice(D2Q9, device, dtype)
-    flow_class = flow_by_name[flow]
+    flow_class, stencil = flow_by_name[flow]
+    lattice = Lattice(stencil, device, dtype)
+    flow_class = flow_class
     flow = flow_class(resolution=resolution, reynolds_number=1, mach_number=0.05, lattice=lattice)
     force = Guo(
         lattice,
@@ -81,8 +83,8 @@ def benchmark(ctx, steps, resolution, profile_out, flow, vtk_out):
     mlups = simulation.step(num_steps=steps)
 
     # write profiling output
-    profile.disable()
     if profile_out:
+        profile.disable()
         stats = pstats.Stats(profile)
         stats.sort_stats('cumulative')
         stats.print_stats()

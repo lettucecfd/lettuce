bc5a7f340a05de9433b4b8e21888058695fd3068
Andreas Krämer <kraemer.research@gmail.com>
2021-04-14 06:07:56 +0200

    require torch>=1.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', "torch>=1.1", "numpy", "matplotlib", "pyevtk"]
+requirements = ['Click>=6.0', "torch>=1.2", "numpy", "matplotlib", "pyevtk"]
 
 setup_requirements = ['pytest-runner', 'pytest']
 

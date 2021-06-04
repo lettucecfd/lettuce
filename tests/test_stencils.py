741af785ea306eeb49a38c9e5e847b833f21a17e
Andreas Krämer <kraemer.research@gmail.com>
2021-06-04 09:48:31 +0200

    nonsense change to test linting

--- tests/test_stencils.py
+++ tests/test_stencils.py
@@ -20,3 +20,16 @@ def test_weights(lattice):
 def test_first_zero(lattice):
     """Test that the zeroth velocity is 0."""
     assert lattice.stencil.e[0] == pytest.approx(np.zeros_like(lattice.stencil.e[0]))
+
+
+def test_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_long_name():
+    pass
+
+
+
+
+
+
+
+
+#sdklsdf

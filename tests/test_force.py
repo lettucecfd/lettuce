2d991502373fa2ebb692d95f1afe9b399ce8df17
Andreas Krämer <30830507+Olllom@users.noreply.github.com>
2022-01-28 14:59:59 +0100

    Fix order of operations and velocity in forcing scheme (#114)
    * fixed order of operations
    
    changed order of basic operations in each timestep as discussed in #103
    
    * changed reporting back to the original order
    
    ... to ensure the results of the last step are reported and i fits the number of the time step (starting condition i = 0)
    
    * incrementing time before reporting
    
    to keep order of operations like source but still report all time steps with the proper number
    
    * fixed initialize_f_neq to use -f_neq as discussed
    
    * fix force test
    
    * correct velocity computation in Lattice;
    
    and avoid duplicate computation of rho
    closes #104 and #90
    
    Co-authored-by: Martin Leonhard Kliemank <49449166+MartinKliemank@users.noreply.github.com>
    Co-authored-by: MartinKliemank <martin.kliemank@gmx.de>

--- tests/test_force.py
+++ tests/test_force.py
@@ -13,14 +13,16 @@ def test_force_guo(ForceType, device):
     lattice = Lattice(D2Q9, dtype=dtype, device=device)
     flow = PoiseuilleFlow2D(resolution=10, reynolds_number=1, mach_number=0.02, lattice=lattice,
                             initialize_with_zeros=True)
+    acceleration_lu = flow.units.convert_acceleration_to_lu(flow.acceleration)
     force = ForceType(lattice, tau=flow.units.relaxation_parameter_lu,
-                      acceleration=flow.units.convert_acceleration_to_lu(flow.acceleration))
+                      acceleration=acceleration_lu)
     collision = BGKCollision(lattice, tau=flow.units.relaxation_parameter_lu, force=force)
     streaming = StandardStreaming(lattice)
     simulation = Simulation(flow=flow, lattice=lattice, collision=collision, streaming=streaming)
-    simulation.step(500)
+    simulation.step(1000)
     # compare with reference solution
-    u_sim = flow.units.convert_velocity_to_pu(lattice.convert_to_numpy(lattice.u(simulation.f)))
+    u_sim = lattice.u(simulation.f, acceleration=acceleration_lu)
+    u_sim = flow.units.convert_velocity_to_pu(lattice.convert_to_numpy(u_sim))
     _, u_ref = flow.analytic_solution(flow.grid)
     fluidnodes = np.where(np.logical_not(flow.boundaries[0].mask.cpu()))
     assert u_ref[0].max() == pytest.approx(u_sim[0].max(), rel=0.005)

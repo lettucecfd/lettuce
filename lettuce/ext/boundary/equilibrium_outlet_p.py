870d0cd928ea58d764d03365f8b8c444f596c288
Mario Bedrunka <mario.bedrunka@gmail.com>
2024-05-28 18:22:53 +0200

    Working test_native_equilibrium_outlet_p.py --use-no-native

--- lettuce/ext/boundary/equilibrium_outlet_p.py
+++ lettuce/ext/boundary/equilibrium_outlet_p.py
@@ -1,38 +1,79 @@
+from typing import List, Optional
+
 import numpy as np
 import torch
 
-from . import AntiBounceBackOutlet
-
 __all__ = ['EquilibriumOutletP']
 
+from ... import Flow, Context
 
-class EquilibriumOutletP(AntiBounceBackOutlet):
+
+class EquilibriumOutletP():
     """Equilibrium outlet with constant pressure.
     """
 
-    def __init__(self, lattice, direction, rho0=1.0):
-        AntiBounceBackOutlet.__init__(self, lattice, direction)
-        self.rho0 = rho0
+    def __init__(self, flow: 'Flow', context: 'Context', direction: List[int], rho_outlet: float = 1.0):
+        self.rho_outlet = context.convert_to_tensor(rho_outlet)
+        self.context = context
 
-    def __call__(self, f):
-        here = [slice(None)] + self.index
-        other = [slice(None)] + self.neighbor
-        rho = self.lattice.rho(f)
-        u = self.lattice.u(f)
-        rho_w = self.rho0 * torch.ones_like(rho[here])
-        u_w = u[other]
-        f[here] = self.lattice.equilibrium(rho_w[..., None], u_w[..., None])[..., 0]
-        return f
+        assert isinstance(direction, list), \
+            f"Invalid direction parameter. Expected direction of type list but got {type(direction)}."
 
-    def make_no_stream_mask(self, f_shape):
-        no_stream_mask = torch.zeros(size=f_shape, dtype=torch.bool, device=self.lattice.device)
-        no_stream_mask[[np.setdiff1d(np.arange(self.lattice.Q), self.velocities)] + self.index] = 1
-        return no_stream_mask
+        assert len(direction) in [1, 2, 3], \
+            f"Invalid direction parameter. Expected direction of of length 1, 2 or 3 but got {len(direction)}."
 
-    def make_no_collision_mask(self, f_shape):
-        no_collision_mask = torch.zeros(size=f_shape[1:], dtype=torch.bool, device=self.lattice.device)
+        assert (direction.count(0) == (len(direction) - 1)) and ((1 in direction) ^ (-1 in direction)), \
+            f"Invalid direction parameter. Expected direction with all entries 0 except one 1 or -1 but got {direction}."
+
+        direction = np.array(direction)
+
+        # select velocities to be bounced (the ones pointing in "direction")
+        self.velocities = np.concatenate(np.argwhere(np.matmul(flow.stencil.e, direction) > 1 - 1e-6), axis=0)
+
+        # build indices of u and f that determine the side of the domain
+        self.index = []
+        self.neighbor = []
+        for i in direction:
+            if i == 0:
+                self.index.append(slice(None))
+                self.neighbor.append(slice(None))
+            if i == 1:
+                self.index.append(-1)
+                self.neighbor.append(-2)
+            if i == -1:
+                self.index.append(0)
+                self.neighbor.append(1)
+        # construct indices for einsum and get w in proper shape for the calculation in each dimension
+        if len(direction) == 3:
+            self.dims = 'dc, cxy -> dxy'
+            self.w = flow.torch_stencil.w[self.velocities].view(1, -1).t().unsqueeze(1)
+        if len(direction) == 2:
+            self.dims = 'dc, cx -> dx'
+            self.w = flow.torch_stencil.w[self.velocities].view(1, -1).t()
+        if len(direction) == 1:
+            self.dims = 'dc, c -> dc'
+            self.w = flow.torch_stencil.w[self.velocities]
+
+    def __call__(self, flow):
+        outlet = [slice(None)] + self.index
+        neighbor = [slice(None)] + self.neighbor
+        rho_outlet = self.rho_outlet * torch.ones_like(flow.rho()[outlet])
+        feq = flow.equilibrium(flow, rho_outlet[..., None], flow.u()[neighbor][..., None])
+        return flow.einsum("q,q->q", [feq, torch.ones_like(flow.f)])
+
+    def make_no_streaming_mask(self, shape: List[int], context: 'Context') -> Optional[torch.Tensor]:
+        no_streaming_mask = context.zero_tensor(shape, dtype=bool)
+        no_streaming_mask[[np.setdiff1d(np.arange(shape[0]), self.velocities)] + self.index] = 1
+        return no_streaming_mask
+
+    def make_no_collision_mask(self, shape: List[int], context: 'Context'):
+        # no_collision_mask = torch.zeros(size=shape[1:], dtype=torch.bool, device=context.device)
+        no_collision_mask = context.zero_tensor(shape, dtype=bool)
         no_collision_mask[self.index] = 1
         return no_collision_mask
 
     def native_available(self) -> bool:
         return False
+
+    def native_generator(self, index: int):
+        pass

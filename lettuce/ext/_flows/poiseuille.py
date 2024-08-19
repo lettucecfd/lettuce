f471edf210ce33923c3384c24d503c3f803ddfed
PhiSpel <philipp.spelten@web.de>
2024-08-19 15:10:14 +0200

    wip - trying to fix solution of poiseuille flow for test_force

--- lettuce/ext/_flows/poiseuille.py
+++ lettuce/ext/_flows/poiseuille.py
@@ -32,41 +32,43 @@ class PoiseuilleFlow2D(ExtFlow):
         x, y = self.grid
         nu = self.units.viscosity_pu
         rho = 1
-        u = torch.stack(
-            (self.acceleration[0] / (2 * rho * nu)
-            * ((y - half_lattice_spacing) * (1 - half_lattice_spacing - y)),
-            self.context.zero_tensor(x.shape)),
-            dim=0
-        )
+        ux = (self.acceleration[0] / (2 * rho * nu)
+              * ((y - half_lattice_spacing) * (1 - half_lattice_spacing - y)))
+        uy = self.context.zero_tensor(self.resolution)
+        u = torch.stack([ux, uy], dim=0)
         p = y * 0 + self.units.convert_density_lu_to_pressure_pu(rho)
         return p, u
 
     def initial_pu(self):
         if self.initialize_with_zeros:
             zeros = self.context.zero_tensor(self.resolution)
+            p = zeros[None, ...]
             u = torch.stack(2*[zeros], dim=0)
-            p = torch.stack([zeros], dim=0)
             return p, u
         else:
-            return self.analytic_solution(self.grid)
+            return self.analytic_solution()
 
     def make_units(self, reynolds_number, mach_number,
                    resolution: List[int]) -> 'UnitConversion':
         return UnitConversion(
             reynolds_number=reynolds_number, mach_number=mach_number,
-            characteristic_length_lu=resolution[0], characteristic_length_pu=1,
+            characteristic_length_lu=resolution[0],
+            characteristic_length_pu=1,
             characteristic_velocity_pu=1
         )
 
     def make_resolution(self, resolution: Union[int, List[int]],
                         stencil: Optional['Stencil'] = None) -> List[int]:
+        if isinstance(resolution, list):
+            assert len(resolution) == self.stencil.d
         if isinstance(resolution, int):
             resolution = [resolution] * self.stencil.d
         return resolution
 
     @property
     def grid(self):
-        xyz = tuple(torch.linspace(0, 1, steps=n,
+        xyz = tuple(torch.linspace(0, 1,
+                                   steps=n,
                                    device=self.context.device,
                                    dtype=self.context.dtype)
                     for n in self.resolution)

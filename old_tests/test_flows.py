0367e26257a85bf18cf7461fc3c1f65285607e1b
PhiSpel <philipp.spelten@web.de>
2024-08-19 09:29:51 +0200

    migrated test_divergence

--- old_tests/test_flows.py
+++ old_tests/test_flows.py
@@ -8,35 +8,6 @@ from lettuce import Obstacle2D, Obstacle3D
 from lettuce.flows.poiseuille import PoiseuilleFlow2D
 from lettuce import Context
 
-# Flows to test
-INCOMPRESSIBLE_2D = [TaylorGreenVortex2D, CouetteFlow2D, PoiseuilleFlow2D, DoublyPeriodicShear2D, DecayingTurbulence]
-INCOMPRESSIBLE_3D = [TaylorGreenVortex3D, DecayingTurbulence]
-
-
-@pytest.mark.parametrize("_stencil", [D2Q9, D3Q27])
-def test_divergence(stencil, dtype_device):
-    dtype, device = dtype_device
-    lattice = Lattice(stencil, dtype=dtype, device=device)
-    flow = DecayingTurbulence(50, 1, 0.05, lattice=lattice, ic_energy=0.5)
-    collision = BGKCollision(lattice, tau=flow.units.relaxation_parameter_lu)
-    streaming = StandardStreaming(lattice)
-    simulation = Simulation(flow=flow, lattice=lattice, collision=collision, streaming=streaming)
-    ekin = flow.units.convert_incompressible_energy_to_pu(
-        torch.sum(lattice.incompressible_energy(simulation.f))) * flow.units.convert_length_to_pu(1.0) ** lattice.D
-
-    u0 = flow.units.convert_velocity_to_pu(lattice.u(simulation.f)[0])
-    u1 = flow.units.convert_velocity_to_pu(lattice.u(simulation.f)[1])
-    dx = flow.units.convert_length_to_pu(1.0)
-    grad_u0 = torch_gradient(u0, dx=dx, order=6).cpu().numpy()
-    grad_u1 = torch_gradient(u1, dx=dx, order=6).cpu().numpy()
-    divergence = np.sum(grad_u0[0] + grad_u1[1])
-
-    if lattice.D == 3:
-        u2 = flow.units.convert_velocity_to_pu(lattice.u(simulation.f)[2])
-        grad_u2 = torch_gradient(u2, dx=dx, order=6).cpu().numpy()
-        divergence += np.sum(grad_u2[2])
-    assert (flow.ic_energy == pytest.approx(lattice.convert_to_numpy(ekin), rel=1))
-    assert (0 == pytest.approx(divergence, abs=2e-3))
 
 
 @pytest.mark.parametrize("_stencil", [D2Q9, D3Q27])

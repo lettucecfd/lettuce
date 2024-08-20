c36485879bc73b7c371d6731c3b084c5355b8124
PhiSpel <philipp.spelten@web.de>
2024-08-20 14:01:16 +0200

    migrated test_vtk_reporter_mask

---
+++ tests/reporter/test_vtk_reporter_mask.py
@@ -0,0 +1,17 @@
+import os
+
+from tests.common import *
+
+def test_vtk_reporter_mask(tmpdir):
+    flow = PoiseuilleFlow2D(context=Context(), resolution=16,
+                            reynolds_number=10, mach_number=0.05)
+    collision = BGKCollision(tau=flow.units.relaxation_parameter_lu)
+    simulation = Simulation(flow, collision, [])
+    vtk_reporter = VTKReporter(interval=1,
+                               filename_base=tmpdir / "output")
+    simulation.reporter.append(vtk_reporter)
+    vtk_reporter.output_mask(simulation)
+    simulation(2)
+    assert os.path.isfile(tmpdir / "output_mask.vtr")
+    assert os.path.isfile(tmpdir / "output_00000000.vtr")
+    assert os.path.isfile(tmpdir / "output_00000001.vtr")

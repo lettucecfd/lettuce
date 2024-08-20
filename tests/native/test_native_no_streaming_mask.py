893d001eec5c2997a71f9d33381b15a57e30a515
PhiSpel <philipp.spelten@web.de>
2024-08-20 11:26:38 +0200

    migrated test_native_no_streaming_mask

---
+++ tests/native/test_native_no_streaming_mask.py
@@ -0,0 +1,21 @@
+from tests.common import *
+
+def test_native_no_streaming_mask():
+    """test if """
+
+    if not torch.cuda.is_available():
+        pytest.skip("Native test skipped")
+
+    context = Context(dtype=torch.float32, device=torch.device("cuda"),
+                      use_native=True)
+    flow = TestFlow(context, 16, 1, 0.01)
+    collision = NoCollision()
+    simulation = Simulation(flow, collision, [])
+    simulation.no_streaming_mask = context.zero_tensor(flow.resolution,
+                                                       dtype=bool)
+
+    f0 = copy(flow.f)
+    simulation(64)
+    f1 = flow.f
+
+    assert torch.isclose(f0, f1).all()

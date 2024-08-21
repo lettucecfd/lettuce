a2764031df830a70ee611a919a2b707bc8fc200e
PhiSpel <philipp.spelten@web.de>
2024-08-21 10:30:47 +0200

    migrated test_opposite

--- old_tests/test_stencils.py
+++ old_tests/test_stencils.py
@@ -2,11 +2,6 @@ import pytest
 import numpy as np
 
 
-def test_opposite(lattice):
-    """Test if the opposite field holds the index of the opposite direction."""
-    assert lattice.e[lattice.stencil.opposite].cpu().numpy() == pytest.approx(-lattice.e.cpu().numpy())
-
-
 def test_symmetry(lattice):
     """Test if the stencil is symmetric"""
     assert sum(sum(lattice.e.cpu().numpy())) == pytest.approx(0.0)

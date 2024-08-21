ca06daab6f9939a19b6f44c50d74188124982772
PhiSpel <philipp.spelten@web.de>
2024-08-21 10:34:56 +0200

    migrated test_first_zero

--- old_tests/test_stencils.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import numpy as np
-
-
-def test_first_zero(lattice):
-    """Test that the zeroth velocity is 0."""
-    assert lattice.stencil.e[0] == pytest.approx(np.zeros_like(lattice.stencil.e[0]))

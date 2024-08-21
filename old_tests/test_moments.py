528b6b916aa74300ad85950859de2d297cf73171
PhiSpel <philipp.spelten@web.de>
2024-08-21 17:04:47 +0200

    migrated test_moment_equilibrium_lallemand

--- old_tests/test_moments.py
+++ old_tests/test_moments.py
@@ -5,18 +5,6 @@ from lettuce._stencil import *
 from lettuce.lattices import Lattice
 
 
-def test_moment_equilibrium_lallemand(dtype_device):
-    dtype, device = dtype_device
-    lattice = Lattice(D2Q9, device, dtype)
-    moments = D2Q9Lallemand(lattice)
-    np.random.seed(1)
-    f = lattice.convert_to_tensor(np.random.random([lattice.Q] + [3] * lattice.D))
-    meq1 = lattice.convert_to_numpy(moments.transform(lattice.equilibrium(lattice.rho(f), lattice.u(f))))
-    meq2 = lattice.convert_to_numpy(moments.equilibrium(moments.transform(f)))
-    same_moments = moments["rho", "jx", "jy", "qx", "qy"]
-    assert meq1[same_moments] == pytest.approx(meq2[same_moments], abs=1e-5)
-
-
 def test_moment_equilibrium_D3Q27Hermite(dtype_device):
     dtype, device = dtype_device
     lattice = Lattice(D3Q27, device, dtype)
---
+++ tests/moments/test_moment_equilibrium_lallemand.py
@@ -0,0 +1,15 @@
+from tests.common import *
+
+
+def test_moment_equilibrium_lallemand(fix_device, fix_dtype):
+    context = Context(fix_device, fix_dtype)
+    stencil = D2Q9()
+    moments = D2Q9Lallemand(stencil, context)
+    np.random.seed(1)
+    flow = TestFlow(context, 10, 1, 0.1, stencil=stencil)
+    meq1 = context.convert_to_ndarray(moments.transform(flow.equilibrium(
+        flow)))
+    meq2 = context.convert_to_ndarray(moments.equilibrium(moments.transform(
+        flow.f), flow))
+    same_moments = moments["rho", "jx", "jy", "qx", "qy"]
+    assert meq1[same_moments] == pytest.approx(meq2[same_moments], abs=1e-5)

b76f85fe10e4d14be9049047f7d75955f8da9a02
PhiSpel <philipp.spelten@web.de>
2024-08-21 10:33:42 +0200

    migrated text_weights

--- old_tests/test_stencils.py
+++ old_tests/test_stencils.py
@@ -2,11 +2,6 @@ import pytest
 import numpy as np
 
 
-def test_weights(lattice):
-    """Test if the sum of all weights equals one."""
-    assert sum(lattice.w.cpu().numpy()) == pytest.approx(1.0)
-
-
 def test_first_zero(lattice):
     """Test that the zeroth velocity is 0."""
     assert lattice.stencil.e[0] == pytest.approx(np.zeros_like(lattice.stencil.e[0]))
--- tests/stencil/test_symmetry.py
+++ tests/stencil/test_symmetry.py
@@ -3,4 +3,4 @@ from tests.common import *
 
 def test_symmetry(fix_stencil):
     """Test if the stencil is symmetric"""
-    assert sum(np.array(fix_stencil.e)) == pytest.approx(0.0)
+    assert np.array(fix_stencil.e).sum() == pytest.approx(0.0)

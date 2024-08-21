d0be6aea356baaac6726b40a5505210251c227f5
PhiSpel <philipp.spelten@web.de>
2024-08-21 10:45:20 +0200

    migrated test_unit.py

---
+++ tests/unit/__init__.py
@@ -0,0 +1,11 @@
+from tests.common import *
+
+
+def create_default_unit_conversion():
+    return UnitConversion(
+        reynolds_number=1000,
+        mach_number=0.05,
+        characteristic_length_pu=2 * np.pi,
+        characteristic_velocity_pu=2,
+        characteristic_length_lu=100,
+        characteristic_density_pu=0.7)

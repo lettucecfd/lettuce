db6cdd919f5007e848a2d2e3568f51c80d986959
PhiSpel <philipp.spelten@web.de>
2024-08-26 10:03:05 +0200

    added PartiallySaturatedBC

--- lettuce/ext/_boundary/__init__.py
+++ lettuce/ext/_boundary/__init__.py
@@ -2,10 +2,12 @@ from .anti_bounce_back_outlet import AntiBounceBackOutlet
 from .bounce_back_boundary import BounceBackBoundary
 from .equilibrium_boundary_pu import EquilibriumBoundaryPU
 from .equilibrium_outlet_p import EquilibriumOutletP
+from .partially_saturated_boundary import PartiallySaturatedBC
 
 __all__ = [
     'AntiBounceBackOutlet',
     'BounceBackBoundary',
     'EquilibriumBoundaryPU',
-    'EquilibriumOutletP'
+    'EquilibriumOutletP',
+    'PartiallySaturatedBC'
 ]

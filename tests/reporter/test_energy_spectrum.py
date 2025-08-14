d8f5451531f8789eb79833ff0dad3ebb8701a5c0
Mario <mario.bedrunka@gmail.com>
2025-08-14 17:10:27 +0200

    Correct TGV unit converter definition and update related test cases

--- tests/reporter/test_energy_spectrum.py
+++ tests/reporter/test_energy_spectrum.py
@@ -25,4 +25,4 @@ def test_energy_spectrum(tmpdir, flowname):
         # check that flow has only one mode
         ek_max = sorted(spectrum, reverse=True)
         assert ek_max[0] * 1e-5 > ek_max[1]
-    assert (energy == pytest.approx(np.sum(spectrum), rel=0.1, abs=0.0))
+    assert (energy == pytest.approx(np.sum(spectrum), rel=1e-4, abs=1e-2))

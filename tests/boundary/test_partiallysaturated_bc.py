0a4a8b7d8cedd5b5e48f24eb8789c98c4ec9fde7
PhiSpel <philipp.spelten@web.de>
2025-08-26 13:19:03 +0200

    ps bc only bounces back neq parts [kruger]

--- tests/boundary/test_partiallysaturated_bc.py
+++ tests/boundary/test_partiallysaturated_bc.py
@@ -21,24 +21,24 @@ def test_partiallysaturated_boundary(fix_stencil, fix_configuration):
     simulation(2)
 
 
-def test_fullysaturated_like_bounceback(fix_stencil, fix_configuration):
+def test_fullysaturated_like_neq_bounceback(fix_stencil, fix_configuration):
     device, dtype, use_native = fix_configuration
     if use_native:
         pytest.skip("This test does not depend on the native implementation.")
     context = Context(device=device, dtype=dtype, use_native=False)
-    flow1 = TestFlow(context, resolution=fix_stencil.d * [16],
-                     reynolds_number=1, mach_number=0.1, stencil=fix_stencil)
-    flow2 = TestFlow(context, resolution=fix_stencil.d * [16],
-                     reynolds_number=1, mach_number=0.1, stencil=fix_stencil)
-    tau = flow1.units.relaxation_parameter_lu
-    mask = context.one_tensor(flow1.resolution, dtype=bool)  # will contain all
+    flow = TestFlow(context, resolution=fix_stencil.d * [16],
+                    reynolds_number=1, mach_number=0.1, stencil=fix_stencil)
+    mask = context.one_tensor(flow.resolution, dtype=bool)  # will contain all
     # points
+    tau = flow.units.relaxation_parameter_lu
     PS_BC = PartiallySaturatedBC(mask, tau, saturation=1.0)
-    f_fullysaturated = PS_BC(flow1)
-    BB_BC = BounceBackBoundary(mask)
-    f_bounced = BB_BC(flow2)
-    assert (f_fullysaturated.cpu().numpy() ==
-            pytest.approx(f_bounced.cpu().numpy()))
+    f_fullysaturated = PS_BC(flow)
+    flow2 = copy(flow)
+    flow2.f = f_fullysaturated
+    fneq_fullysaturated = flow2.equilibrium(flow2) - flow2.f
+    fneq_bounceback = flow.f-flow.equilibrium(flow)[flow.stencil.opposite]
+    assert (fneq_bounceback.cpu().numpy() ==
+            pytest.approx(fneq_fullysaturated.cpu().numpy()))
     
 
 

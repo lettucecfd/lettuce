121359d2b7eed2c21ae81446645ac306e15e3c59
MB <116886100+MaxBille@users.noreply.github.com>
2026-03-25 15:29:43 +0100

    270 efficient hwbb and ibb (#287)
    * started translating the MP2_release cylinderFlow to current lettuce master.
    
    including: obstacle_cylinder.py, example-Simulation-script, HWBB and IBB boundary conditions, MEA for Bounce back boundaries, drag and lift observables...
    
    * started translating the MP2_release cylinderFlow to current lettuce master.
    
    including: obstacle_cylinder.py, example-Simulation-script, HWBB and IBB boundary conditions, MEA for Bounce back boundaries, drag and lift observables...
    
    * progress on cylinder flow with efficient bounce back boundaries (ported from MP2 and MA).
    obstacle_cylinder.py implements the flow, the boundary classes are implemented in the respective files under /_boundary
    WorkInProgress, see ToDOs
    
    * Moved all files for advanced_cylinder-example with efficient bounce back boundaries to folder examples/advanced_projects/efficient_bounce_back_obstacle.
    Includes boundary conditions, obstacle_cylinder.py and placeholders for simulation-scripts for Re40-300 and Re3900, and a jupyter-notebook to run the scripts interactively
    
    _simulation.py: added comments for clarification
    
    * temp_comit: progress of today
    
    * debugging before tests of first cylinder flow
    
    * commit before trying to find error in master regarding solid boundaries
    
    * fwbb and hwbb seem to be running now... Ibb1 gives NaN, but code runs...
    
    * ibb1 seems to work, now has to be tested with observables etc. to compare to MP2 and cylinder2025 results!
    
    * implemented Drag, Lift and more print-statements for cylinder-flow example to validate against MP2 and cylinder-paper data; to be tested...
    
    * corrected some stuff for better compatibility with master branch:
    - comments
    - print-statements
    - ToDos
    - revert changes in .ipynbs (less clutter in later PR)
    
    * tested FWBB, HWBB, IBB against MP2 code. Visually identical results, numerically there are diffs. of ~1% in mean Drag... this might be due to the new pre/ost-boundary strategy of lettuce 2025.
    Needs further investigation (issue already open).
    Further improvements and corrections:
    - implemented lateral walls option (for FWBB)
    - removed some comments and old code sections.
    NEXT will be output/data/vtk cleaning and example .ipynb script for execution
    
    * tidying of 01a_script_cylinder_lowRe.py
    - methods for force coefficient plotting and analysis were outsourced to data_processing_and_plotting.py
    - files for NaN-, HighMa-, and Progress-Reporter created (but work in progress)
    
    * corrected drag- and lift-plotting, peak-finding and plotting of peak-finding
    
    * implemented profile-reporter, added profile-reference-data, implemented profile-data-post-processing und -plotting.
    implemented advanced vtk reporter (3D with step start and end and 2D for XY-slices)
    
    * minor changes
    
    * unified cylinder simulation script; minor corrections in files, removal of un-implemented reporter-files.
    
    * final touches and prlimilary test for Re200 and Re3900 run of cylinder flow with 01_script_cylinder_simulation.py through ipynb: 00_run_parameterized_project.ipynb
    
    added "show" parameter to plotting-reporters methods, to not only plot and save but also show the plotted data
    
    * minor changes: spelling
    
    * minor changes: removed Reporter from import list of observables_force_coefficients.py
    
    * FOR REVIEW! code formatting, minor repairs, comments, desctiptions, cleanup
    
    * small changes as review requirements (@PhiSpel)
    
    * light reformatting for typing and PEP8
    
    * major reformatting for typing, directory-structure and comment-cleaning, before next review cycle
    
    * Chore: Remove local jupyter dependency
    
    * chore: revert uv.lock to match master revision format
    
    * small corrections in equilibrium_outlet_p.py to make it consistent with master branch
    
    * small corrections in equilibrium_outlet_p.py to make it consistent with master branch (2)

---
+++ examples/advanced_projects/efficient_bounce_back_obstacle/__init__.py
@@ -0,0 +1,11 @@
+from examples.advanced_projects.efficient_bounce_back_obstacle.boundary.solid_boundary_data import SolidBoundaryData
+from examples.advanced_projects.efficient_bounce_back_obstacle.boundary.fullway_bounce_back_boundary import FullwayBounceBackBoundary
+from examples.advanced_projects.efficient_bounce_back_obstacle.boundary.halfway_bounce_back_boundary import HalfwayBounceBackBoundary
+from examples.advanced_projects.efficient_bounce_back_obstacle.boundary.linear_interpolated_bounce_back_boundary import LinearInterpolatedBounceBackBoundary
+
+__all__ = [
+    'FullwayBounceBackBoundary',
+    'HalfwayBounceBackBoundary',
+    'LinearInterpolatedBounceBackBoundary',
+    'SolidBoundaryData'
+]
---
+++ examples/advanced_projects/efficient_bounce_back_obstacle/auxiliary_code/helperCode.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+
+class Logger(object):
+    """logging functionality proposed by P.Spelten to log stdout to file"""
+    def __init__(self, outdir):
+        self.terminal = sys.stdout
+        self.filename = os.path.join(outdir, "log")
+
+    def write(self, message):
+        self.terminal.write(message)
+        f = open(self.filename, "a")
+        f.write(message)
+        f.close()
+
+    def flush(self):
+        # this flush method is needed for python 3 compatibility.
+        # This handles the flush command by doing nothing.
+        # you might want to specify some extra behavior here.
+        pass
